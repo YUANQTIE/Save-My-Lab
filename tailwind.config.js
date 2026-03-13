30e02dc39f87bf3fb9816b5532646d440b0ffb82
Ayesa <ayesatullao@gmail.com>
2026-03-13 16:58:51 +0800

    login

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+        backgroundImage: {
+        'bg-pattern': "url('images/dlsubg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
