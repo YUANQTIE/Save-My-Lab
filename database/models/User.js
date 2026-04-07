03368cc8bb3e6926e6588969eb54ac6750a483dc
YUANQTIE <yuanpanlilio@gmail.com>
2026-04-07 22:49:49 +0800

    pic

--- database/models/User.js
+++ database/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     username: {type: String, required: true, unique: true},
     bio: {type: String, default: ""},
-    profile_picture: { type: String, required: true, default: "/images/user.png" },
+    profile_picture: { type: String, required: true, default: "/images/blank_picture.png" },
     date_created: {type: Date, required: true},
     password: {type: String, required: true},
     last_login: {type: Date},
